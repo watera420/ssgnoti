84fd82a3ae2357c6ac7b97fc2f7a7073129fd723
LEE MIN JUNG <lmj0645@naver.com>
2013-06-11 21:05:12 +0900

    이민정이민정

--- ssgnoti/src/com/shinsegae/android/ssgnoti/DeptSetlActivity.java
+++ ssgnoti/src/com/shinsegae/android/ssgnoti/DeptSetlActivity.java
@@ -1,14 +1,13 @@
 package com.shinsegae.android.ssgnoti;
 
 import android.app.Activity;
-import android.content.Intent;
-import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
+import android.view.Window;
 import android.widget.AdapterView;
-import android.widget.ListView;
 import android.widget.AdapterView.OnItemClickListener;
 import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 public class DeptSetlActivity extends Activity implements OnItemClickListener {
 	String[] msg = new String[] { "온라인백화점", "백화점", "POS&결제", "인사재무",
@@ -19,8 +18,9 @@ public class DeptSetlActivity extends Activity implements OnItemClickListener {
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
+		requestWindowFeature(Window.FEATURE_NO_TITLE); //액션바없앰
 		setContentView(R.layout.activity_deptset);
-		
+
 		ListView lv = (ListView) findViewById(R.id.testListView);
 
 		adapter = new ArrayAdapter<String>(this,
@@ -32,21 +32,16 @@ public class DeptSetlActivity extends Activity implements OnItemClickListener {
 
 	}
 
-	
 	@Override
-	public void onItemClick(AdapterView<?> parent, View arg1, int arg2, long arg3) {
+	public void onItemClick(AdapterView<?> parent, View arg1, int arg2,
+			long arg3) {
 
-	     
-	    // 클릭된 아이템의 포지션을 이용해 스트링어레이에서 아이템을 꺼내온다.
-	    
-		Intent intent = new Intent(this, MainActivity.class);
-		intent.putExtra("dept", adapter.getItem(arg2));	
-		
-		startActivity(intent);
+		// 클릭된 아이템의 포지션을 이용해 스트링어레이에서 아이템을 꺼내온다.
+
+		String selectedDept = adapter.getItem(arg2);
+		new DataSource(this).save(selectedDept);
+		finish();
 
 	}
-	
-	
-	
-	
+
 }
--- ssgnoti/src/com/shinsegae/android/ssgnoti/MainActivity.java
+++ ssgnoti/src/com/shinsegae/android/ssgnoti/MainActivity.java
@@ -15,30 +15,18 @@ import android.app.Activity;
 import android.app.AlarmManager;
 import android.app.PendingIntent;
 import android.content.Intent;
-import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.os.SystemClock;
-import android.util.Log;
 import android.view.View;
+import android.view.Window;
 import android.widget.AdapterView;
 import android.widget.AdapterView.OnItemClickListener;
 import android.widget.ListView;
 import android.widget.TextView;
-import android.widget.Toast;
 
 public class MainActivity extends Activity {
 	// All static variables
 	static final String URL = "http://1.234.89.248/1/birthinfo.xml";
-	// XML node keys
-	static final String KEY_EMPINFO = "employee"; // parent node
-	static final String KEY_ID = "id"; // 사번
-	static final String KEY_NAME = "name"; // 이름
-	static final String KEY_DEPT = "dept"; // 부서
-	static final String KEY_BIRTH = "birth"; // 생년월일
-	static final String KEY_PN = "pn"; // 핸드폰번호
-	static final String KEY_POSITION = "position"; // 직책
-	static final String KEY_THUMB_URL = "thumb_url"; // 이미지 URL
-	static final String KEY_D_DAY = "dday"; // D-DAY
 	static String KEY_SORT = "000"; // D-DAY
 
 	Calendar yDay = Calendar.getInstance();
@@ -46,25 +34,44 @@ public class MainActivity extends Activity {
 	ListView list;
 	LazyAdapter adapter;
 	ArrayList<HashMap<String, String>> songsList;
-	private Toast mToast;
 
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
+		requestWindowFeature(Window.FEATURE_NO_TITLE); //액션바없앰
 		setContentView(R.layout.activity_main);
 
-		Intent intent2 = getIntent();
-		String deptsetl = intent2.getStringExtra("dept");
-		TextView dept2 = (TextView) findViewById(R.id.dept2);
-		dept2.setText(deptsetl);
+		// 부서 코드 체크
+		String selectedCode = loadSelectedCode();
+		if (selectedCode == null) {
+			showDeptList();
+			return;
+		} else {
+			
+			refreshList();
+			
+		}
 
+	}
+
+	public void refreshList() {
+		loadData();
+		
+		sort();
+		
+		showList();
+		
+		alarm();
+	}
+
+	public void loadData() {
 		songsList = new ArrayList<HashMap<String, String>>();
 
 		XMLParser parser = new XMLParser();
 		String xml = parser.getXmlFromUrl(URL); // getting XML from URL
 		Document doc = parser.getDomElement(xml); // getting DOM element
 
-		NodeList nl = doc.getElementsByTagName(KEY_EMPINFO);
+		NodeList nl = doc.getElementsByTagName("employee");
 		// looping through all song nodes <song>
 		for (int i = 0; i < nl.getLength(); i++) {
 			// creating new HashMap
@@ -72,34 +79,37 @@ public class MainActivity extends Activity {
 			Element e = (Element) nl.item(i);
 			// adding each child node to HashMap key => value
 
-			String a = parser.getValue(e, KEY_DEPT);
+			String a = parser.getValue(e, "dept");
 
+			String deptsetl = loadSelectedCode();
 			if (a.equals(deptsetl)) {
-				map.put(KEY_ID, parser.getValue(e, KEY_ID));
-				map.put(KEY_NAME, parser.getValue(e, KEY_NAME));
-				map.put(KEY_DEPT, parser.getValue(e, KEY_DEPT));
-				String date = parser.getValue(e, KEY_BIRTH);
-				map.put(KEY_BIRTH, date);
-				map.put(KEY_PN, parser.getValue(e, KEY_PN));
-				map.put(KEY_POSITION, parser.getValue(e, KEY_POSITION));
-				map.put(KEY_THUMB_URL, parser.getValue(e, KEY_THUMB_URL));
+				map.put("id", parser.getValue(e, "id"));
+				map.put("name", parser.getValue(e, "name"));
+				map.put("dept", parser.getValue(e, "dept"));
+				String date = parser.getValue(e, "birth");
+				map.put("birth", date);
+				map.put("pn", parser.getValue(e, "pn"));
+				map.put("position", parser.getValue(e, "position"));
+				map.put("thumb_url", parser.getValue(e, "thumb_url"));
 
 				Integer diffDay = DateUtil.getDDay(date);
 				int aaa = 3 - diffDay.toString().length();
 				String sDiffDay = diffDay.toString();
 				if (aaa > 0) {
 					for (int c = 0; c < aaa; c++) {
-						sDiffDay = "0" + diffDay.toString();
+						sDiffDay = "0" + sDiffDay;
 					}
 				}
-				map.put(KEY_D_DAY, "D-" + String.valueOf(diffDay));
+				map.put("dday", "D-" + String.valueOf(diffDay));
 				map.put(KEY_SORT, sDiffDay);
 
 				// adding HashList to ArrayList
 				songsList.add(map);
 			}
 		}
+	}
 
+	public void sort() {
 		// 이름(KEY_NAME)으로 오름차순 리스트정렬 함수 (해쉬맵)
 		class MapComparator implements Comparator<Map<String, String>> {
 			private final String key;
@@ -110,14 +120,16 @@ public class MainActivity extends Activity {
 
 			public int compare(Map<String, String> first,
 					Map<String, String> second) {
-				// TODO: Null checking, both for maps and values
 				String firstValue = first.get(key);
 				String secondValue = second.get(key);
 				return firstValue.compareTo(secondValue);
 			}
 		}
+		
 		Collections.sort(songsList, new MapComparator(KEY_SORT)); // 오름차순 리스트정렬
+	}
 
+	public void showList() {
 		list = (ListView) findViewById(R.id.list);
 
 		// Getting adapter by passing xml data ArrayList
@@ -131,7 +143,6 @@ public class MainActivity extends Activity {
 			public void onItemClick(AdapterView<?> parent, View view,
 					int position, long id) {
 
-				Log.i("TTTT", "songsList.get(position) : " + songsList);
 				Intent intent = new Intent(MainActivity.this,
 						DetailActivity.class);
 				intent.putExtra("songsList",
@@ -141,9 +152,12 @@ public class MainActivity extends Activity {
 
 			}
 		});
+	}
 
-		// 알람매니저
-
+	/**
+	 * 알람매니저
+	 */
+	public void alarm() {
 		AlarmManager am = (AlarmManager) getSystemService(ALARM_SERVICE);
 		Intent intent = new Intent(this, AlarmService_Service.class);
 
@@ -153,9 +167,8 @@ public class MainActivity extends Activity {
 
 		long firstTime = SystemClock.elapsedRealtime();
 		firstTime += 1000;
-		// Alarm을 설정합니다. 10초마다 어떤 일을 반복해서 하고 싶어요
-		// firstTime += 1000;
 
+		// Alarm을 설정합니다. 10초마다 어떤 일을 반복해서 하고 싶어요
 		am.setRepeating(AlarmManager.ELAPSED_REALTIME_WAKEUP, firstTime,
 				(24 * 1000 * 3600), sender);
 
@@ -165,42 +178,29 @@ public class MainActivity extends Activity {
 
 		c.add(Calendar.SECOND, 10);
 
-		/*
-		 * int mHour = c.get(Calendar.HOUR_OF_DAY); int mMinute =
-		 * c.get(Calendar.MINUTE); int mSecond = c.get(Calendar.SECOND);
-		 */
-
 		am.set(AlarmManager.RTC, c.getTimeInMillis(), sender);
-
-		/*
-		 * if (mToast != null) { mToast.cancel(); } mToast =
-		 * Toast.makeText(this, "현재 시간" + mHour + ":" + mMinute + ":" + mSecond,
-		 * Toast.LENGTH_LONG); mToast.show();
-		 */
-
 	}
 
-	private Integer getDday(String date) {
-		String yyyy = date.substring(0, 4);
-		String mm = date.substring(5, 7);
-		String dd = date.substring(8, 10);
-		int i_yyyy = Integer.parseInt(yyyy);
-		int i_mm = Integer.parseInt(mm) - 1;
-		int i_dd = Integer.parseInt(dd);
-
-		yDay.set(2013, i_mm, i_dd);
-
-		long diffSec = (yDay.getTimeInMillis() - toDay.getTimeInMillis()) / 1000; // 초
-		Integer diffDay = (int) (diffSec / (60 * 60 * 24));
-		return diffDay;
+	public String loadSelectedCode() {
+		String deptsetl = new DataSource(this).getSelectedDept();
+		TextView dept2 = (TextView) findViewById(R.id.dept2);
+		dept2.setText(deptsetl);
+		return deptsetl;
 	}
 
 	public void dept_btn(View v) {
-		// TODO Auto-generated method stub
-		Intent intent = new Intent(this, DeptSetlActivity.class);
+		showDeptList();
+	}
 
-		// intent.putExtra("phone", phone.getText().toString());
-		startActivity(intent);
+	public void showDeptList() {
+		Intent intent = new Intent(this, DeptSetlActivity.class);
+		startActivityForResult(intent, 0);
+	}
+	
+	@Override
+	protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+		super.onActivityResult(requestCode, resultCode, data);
+		refreshList();
 	}
 
 }
